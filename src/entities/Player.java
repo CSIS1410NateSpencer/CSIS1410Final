d78be375a629f6dda0625715180adcfb53f3aba9
SpencerIsaacson <spencer_isaacson@yahoo.com>
2015-04-26 16:41:26 -0600

    Deleting that stuff to return to other branch
    Just for now

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,6 +1,5 @@
 package entities;
 
-import entities.playerState.PlayerState;
 import finalProject.Direction;
 import finalProject.Game;
 import graphics.Animation;
@@ -17,15 +16,16 @@ public class Player extends Fighter {
 	private AnimationSet idles;
 	private Animation praise = new Animation("src/images/player/praise_the_sun.png",10);
 	
-	private PlayerState currentPlayerState;
+	
 	Vector2 startingPoint = new Vector2(2175,360);
 	public Player(){
 		initializeHealth(16);
 		direction = Direction.Down;
-		
-		
+		walks = AnimationSet.loadAnimations("player/walking",8);
+		idles = AnimationSet.loadAnimations("player/idle",8);
 		setAttacks(AnimationSet.loadAnimations("player/attack",9));
-		
+		damage = AnimationSet.loadAnimations("player/dmg",4);
+		die = AnimationSet.loadAnimations("player/die",14);
 		setCurrentAnimationSet(idles);
 		sprite = getCurrentAnimationSet().get(direction).currentSprite();
 		getCollider().setWidth(sprite.getWidth());
@@ -57,32 +57,61 @@ public class Player extends Fighter {
 
 	@Override
 	public void update() {
-		getPlayerState().update();
 		sprite = getCurrentAnimationSet().get(direction).currentSprite();
+		
+		if(getCurrentAnimationSet().get(direction).isFinished() == true) {
+			if(getCurrentAnimationSet() == getAttacks())
+				createAttackEntity(75,95,85);
+			if(getCurrentAnimationSet() == die) {
+				Game.stateManager.next();
+			}
+			setCurrentAnimationSet(idles);
+		}
+		
+		if(getCurrentAnimationSet() == idles || getCurrentAnimationSet() == walks){
+			if(Game.input.isPressed(KeyEvent.VK_SPACE))
+					attack();
+			else
+				move();
+		}
+		if(Enemy.enemies == 0) {
+			sprite = praise.currentSprite();
+			if(praise.isFinished()) {
+				System.out.println("YIPPEE!");
+				Game.stateManager.next();
+			}
+		}
+		
+		
 	}
 
-
+	protected void createAttackEntity(int distanceOut, int wide, int tall) {
+		Vector2 directionPoint = direction.getPoint();
+		Vector2 attackPosition = position.add(directionPoint.multiply(distanceOut));
+		attackPosition.x += getCollider().getWidth() / 2;
+		attackPosition.y += getCollider().getHeight() / 2;
+		
+		int width = (int)(Math.abs(directionPoint.x * tall) + Math.abs(directionPoint.y * wide));
+		int height = (int)(Math.abs(directionPoint.y * tall) + Math.abs(directionPoint.x * wide));
+		attackPosition.x -= width / 2;
+		attackPosition.y -= height / 2;
+		
+		new Attack(this, attackPosition,width,height);
+	}
 
 	@Override
 	void takeDamage(int amount) {
 		if(getCurrentAnimationSet() != damage) {
-			setPlayerState(takeDamageState);
+			super.takeDamage(amount);
+			damage.get(direction).play();
+			Game.audio.play("player_hurt.wav");
 		}
 	}
 	
 	@Override
 	void die() {
 		super.die();
+		Game.audio.play("player_die.wav");
 		
-		
-	}
-
-	public PlayerState getPlayerState() {
-		return currentPlayerState;
-	}
-
-	public void setPlayerState(PlayerState playerState) {
-		this.currentPlayerState = playerState;
-		playerState.begin();
 	}
 }
