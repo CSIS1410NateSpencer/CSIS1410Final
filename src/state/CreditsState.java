a63ad2b2f19e891b74d877c5e616adccf04b035c
thatsscience <nathan.ivy@outlook.com>
2015-04-28 12:16:08 -0600

    credits state update and splash update

--- src/state/CreditsState.java
+++ src/state/CreditsState.java
@@ -1,6 +1,7 @@
 package state;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
@@ -12,13 +13,38 @@ public class CreditsState extends State{
 	int yOffset = starterYOffset;
 	int minYOffset = -450;
 	String[] credits = {
-			"                                                                                                                                                              Credits                         ",
-			"Main character sprites: Nate Ivy",
-			"Programmer: Nate Ivy",
-			"Programmer: Spencer Isaacson",
-			"Sound Effects Recorded By : Spencer Isaacson",
-			"Background music downloaded from sounddogs.com",
-			"Additional sprites and tile sets downloaded from spriters-resource.com",
+			"",
+			"     --------------------------------------------------------------------------------------",
+			"                                                    Credits",
+			"     --------------------------------------------------------------------------------------",
+			"",			
+			"                                   Programmer : Nate Ivy",
+			"                                   Programmer : Spencer Isaacson",			
+			"",
+			"",
+			"     --------------------------------------------------------------------------------------",
+			"                                                    AUDIO",
+			"     --------------------------------------------------------------------------------------",
+			"",
+			"        Sound Effects Recorded By : Spencer Isaacson",
+			"                        Background Music : sounddogs.com",
+			"",
+			"",
+			"     --------------------------------------------------------------------------------------",
+			"                                                GRAPHICS",
+			"     --------------------------------------------------------------------------------------",
+			"",
+			"                Main Character Sprites : Nate Ivy",
+			"        Additional Sprites and Tiles : spriters-resource.com",
+			"",
+			"",
+			"     --------------------------------------------------------------------------------------",
+			"                                          SPECIAL THANKS",
+			"     --------------------------------------------------------------------------------------",
+			"",
+			"                                                        Eitr",
+			"                                                  Dark Souls",
+			"                                                Video Games",
 			"",
 			"",
 			"",
@@ -38,7 +64,12 @@ public class CreditsState extends State{
 			"",
 			"",
 			"",
-			"                                                                                                                                                            THE END",
+			"",
+			"",
+			"",
+			
+			"                                                    THE END",
+			
 			};
 	public CreditsState(Game game) {
 		super(game);
@@ -61,12 +92,14 @@ public class CreditsState extends State{
 	@Override
 	public void render(Graphics g) {
 		g.setColor(Color.WHITE);
+		g.setFont(new Font("arial", Font.BOLD, 30));
 		for (int i = 0; i < credits.length; i++) {
 			g.drawString(credits[i], 25, i * 32 + (int)yOffset);
 		}
 		
 		if(yOffset <= minYOffset)
-			g.drawString("Press the spacebar to restart", 440, 430);
+			
+			g.drawString("Press the spacebar to restart", 301, 430);
 	}
 
 	@Override
